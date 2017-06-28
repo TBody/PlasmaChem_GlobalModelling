1710cddb9585b03ffab8df8bd7179dd9c51338fb
TBod <thomas.body@outlook.com>
2017-06-28 14:31:17 +0100

    Deleted comment

--- OutputPlots/Comp_scan_plot_fractions.m
+++ OutputPlots/Comp_scan_plot_fractions.m
@@ -5,8 +5,6 @@
 % Needs to have 'New scan' folder on path
 % See main README to set-up this folder
 
-% Testing git tracking
-
 % Load composition scan savestates% Computational
 load 'Composition_scan_10mTorr_500W.mat'
 
